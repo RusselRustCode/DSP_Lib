27b0f44a737340de97bc51e5bb8dec0f8dad3763
Ruslan Rustamov <russelrust@RustLapTop.localdomain>
2026-02-09 18:06:29 +0500

    Ïmplemented IDFT and IFFT

--- src/fft.c
+++ src/fft.c
@@ -1,4 +1,4 @@
-#define _USE_MATH_DEFINES
+#define _GNU_SOURCE // для компилятора Clang нужно использовать _GNU_SOURCE, а не USE_MATH_DEFINES, который подходит для GCC или MSVC
 #include <dsp.h>
 #include <math.h>
 #include <stdio.h>
@@ -57,7 +57,7 @@ dsp_complex_numbers* dsp_vector_fft(const dsp_complex_numbers* data, size_t size
     for(int k = 0; k < half_size; k++){
         dsp_complex_numbers twiddleFactor = {
             cosf(2 * M_PI * k / size),
-            sinf(2 * M_PI * k / size)
+            -sinf(2 * M_PI * k / size)
         };
         dsp_complex_numbers twiddleOdd = complex_mul(odd_fft[k], twiddleFactor);
         output[k] = complex_add(even_fft[k], twiddleOdd);
@@ -65,15 +65,73 @@ dsp_complex_numbers* dsp_vector_fft(const dsp_complex_numbers* data, size_t size
     }
     free(even_fft);
     free(odd_fft);
+    
     return output;
 
 }
 
-int dsp_ifft(dsp_complex_numbers* input, dsp_complex_numbers* out, size_t n){
-    return 0;
+dsp_complex_numbers* dsp_ifft(dsp_complex_numbers* input, size_t size){
+    if(size == 0 || (size & (size - 1)) != 0){
+        return NULL;
+    }
+
+    if(size == 1){
+        dsp_complex_numbers* out = malloc(sizeof(dsp_complex_numbers));
+        if(out){
+            out[0] = input[0];
+        }
+        return out; // Если вернем просто input, то программа может просто упасть, т.к. указатель будет на стеке, а нужно, чтобы было на куче
+    }
+    size_t half_size = size / 2;
+
+    dsp_complex_numbers* even_orig = malloc(sizeof(dsp_complex_numbers) * size);
+    dsp_complex_numbers* odd_orig = malloc(sizeof(dsp_complex_numbers) * size);
+    if(!even_orig || !odd_orig){
+        free(even_orig);
+        free(odd_orig);
+        return NULL;
+    }
+
+    for(int i = 0; i < half_size; i++){
+        even_orig[i] = input[2 * i];
+        odd_orig[i] = input[2 * i + 1];
+    }
+
+    dsp_complex_numbers* even = dsp_ifft(even_orig, half_size);
+    dsp_complex_numbers* odd = dsp_ifft(odd_orig, half_size);
+
+    free(even_orig);
+    free(odd_orig);
+
+    dsp_complex_numbers* output = malloc(sizeof(dsp_complex_numbers) * size);
+    for(int n = 0; n < half_size; n++){
+        dsp_complex_numbers twiddleFactor = {
+            cosf(2 * M_PI * n / size) / size,
+            sinf(2 * M_PI * n / size) / size
+        };
+        dsp_complex_numbers twiddleOdd = complex_mul(odd[n], twiddleFactor);
+        output[n] = complex_add(even[n], twiddleOdd);
+        output[n + half_size] = complex_sub(even[n], twiddleOdd);
+    }
+    
+    return NULL;
 }
 
-int dsp_idft(const dsp_vector_t* input, dsp_complex_numbers* out, size_t n){
+int dsp_idft(const dsp_vector_t* input, dsp_complex_numbers* out, size_t size){
+    if(!input || !out){
+        return -1;
+    }
+
+    for(int n = 0; n < size; n++){
+        out[n].real = 0.0f;
+        out[n].imag = 0.0f;
+        for(int i = 0; i < size; i++){
+            out[n].real += input->data[i] * cosf(2 * M_PI * (float)(n * i) / (float)size);
+            out[n].imag += input->data[i] * sinf(2 * M_PI * (float)(n * i) / (float)size);
+        }
+        out[n].real /= size;
+        out[n].imag /= size;
+    }
     return 0;
 }
 
